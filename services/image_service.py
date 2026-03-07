4c1396284028735bc9b4e8fba4888f962512ef70
rojas <victor.rojas@utp.edu.co>
2026-03-06 23:52:21 -0500

    Initialising methods as static for alert resolution.

--- services/image_service.py
+++ services/image_service.py
@@ -1,31 +1,36 @@
 from PIL import Image
-from core.project import Project, Layer
+from core.project import Project
+
 
 class ImageService:
     """Handles image operations and project creation."""
 
-    def create_blank_project(self, width=800, height=600):
-        """Make a blank layer with given width and height."""
+    @staticmethod
+    def create_blank_project(width=800, height=600):
         return Project(width=width, height=height)
 
-    def open_image_project(self, path):
-        """Open an image file at given path anc return in RGBA format."""
+    @staticmethod
+    def open_image_project(path):
         image = Image.open(path).convert("RGBA")
-
         return Project(image=image)
 
-    def set_layer_visibility(self, layer, visible):
+    @staticmethod
+    def set_layer_visibility(layer, visible):
         layer.visible = visible
 
-    def set_layer_opacity(self, layer, opacity):
-        """Applay the opacity of the given layer and set it visible."""
+    @staticmethod
+    def set_layer_opacity(layer, opacity):
+        """Apply the opacity of the given layer and set it visible."""
         layer.opacity = opacity
         alpha_factor = opacity / 100.0
 
         if hasattr(layer, 'original_image') and layer.original_image:
             img = layer.original_image.convert("RGBA")
-            r, g, b, a = img.split()
 
-            # Multiply the alpha channel by the opacity factor.
-            new_alpha = a.point(lambda p: int(p * alpha_factor))
-            layer.image = Image.merge("RGBA", (r, g, b, new_alpha))
+            alpha = img.getchannel('A')
+            new_alpha = alpha.point(lambda p: int(p * alpha_factor))
+
+            temp_img = img.copy()
+            temp_img.putalpha(new_alpha)
+
+            layer.image = temp_img
